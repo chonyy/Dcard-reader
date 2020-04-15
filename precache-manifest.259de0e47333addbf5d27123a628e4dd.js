59cb2da57aa1517d8ce2ab368beb894c53765850
Tony Chou <tcheon8788@gmail.com>
2020-04-16 02:08:31 +0800

    Updates

---
+++ precache-manifest.259de0e47333addbf5d27123a628e4dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cd34370a388ad60c6ebc47b4d0bc0a",
+    "url": "/Dcard-reader/index.html"
+  },
+  {
+    "revision": "9f7cb5193791400e967c",
+    "url": "/Dcard-reader/static/css/main.13414fe5.chunk.css"
+  },
+  {
+    "revision": "ff1ed9c11f0bfe213459",
+    "url": "/Dcard-reader/static/js/2.d32d5d76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dcard-reader/static/js/2.d32d5d76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7cb5193791400e967c",
+    "url": "/Dcard-reader/static/js/main.58aefe48.chunk.js"
+  },
+  {
+    "revision": "3357d76913ff5b331245",
+    "url": "/Dcard-reader/static/js/runtime-main.69862144.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Dcard-reader/precache-manifest.259de0e47333addbf5d27123a628e4dd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dcard-reader/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
